c6cf05b695467dfd3a204f3f573dc2a8639a304e
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2023-08-15 21:46:16 +0000

    fix: deprecated method usage

--- tests/mui/v6/integrations/flask/test_sort_model.py
+++ tests/mui/v6/integrations/flask/test_sort_model.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 def test_parse_grid_sort_model_from_flask_request(sort_model: GridSortModel) -> None:
     key = "sort_model[]"
     with app.app_context():
-        json = dumps([item.dict() for item in sort_model])
+        json = dumps([item.model_dump(mode="json") for item in sort_model])
         query_str = quote(json)
         with app.test_request_context(
             path=(f"/?{key}={query_str}"),
